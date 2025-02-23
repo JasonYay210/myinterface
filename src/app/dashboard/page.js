8b6b6faa7349bceaa42039e42c9a552d6a84ba35
Navi3321 <ihl3321@g.rit.edu>
2025-02-22 21:55:46 -0500

    yes

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,8 +1,11 @@
+import Hero from "@/components/Hero";
+
 export default function DashboardPage() {
     return (
       <div>
         <h1>Welcome to the Dashboard!</h1>
         <p>You are now signed in.</p>
+        <Hero />
       </div>
     );
   }
