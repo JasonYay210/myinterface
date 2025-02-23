dd9049d79284fb025b3eea0665f63b30d585e40f
Jason <JasonYay210@gmail.com>
2025-02-22 23:08:13 -0500

    main, made creation

---
+++ src/mockup-creation/creation.js
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react';
+
+export default function MockupCreationPage() {
+  return (
+    <div>
+      <h1>Mockup Creation</h1>
+      <p>Welcome to the mockup creation page!</p>
+    </div>
+  );
+}
