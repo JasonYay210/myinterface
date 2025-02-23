b9bdcfceb177dc7c2f84dabf0e156361bb2f0f67
Jason <JasonYay210@gmail.com>
2025-02-23 00:25:13 -0500

    main, layout new user now populates doc in firebase

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,8 @@ import {
 import { useRouter } from 'next/navigation'; // Use `next/navigation` for App Router
 import { useEffect } from 'react'; // Add this hook
 import { usePathname } from 'next/navigation';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { db } from '../../firebase';
 import './globals.css';
 
 export default function RootLayout({ children }) {
@@ -33,11 +35,27 @@ export default function RootLayout({ children }) {
 }
 
 function Header() {
-  const { isSignedIn } = useUser();
+  const { isSignedIn, user } = useUser();
   const router = useRouter();
   const pathname = usePathname(); // Get the current route
 
   useEffect(() => {
+    const createUserDocument = async () => {
+      if (isSignedIn && user) {
+        const userRef = doc(db, 'users', user.id); // User document reference by user ID
+        const userDoc = await getDoc(userRef); // Check if the document exists
+
+        if (!userDoc.exists()) {
+          // If the user document doesn't exist, create it
+          await setDoc(userRef, {
+            // empty for now later we will populate it with button info
+          });
+        }
+      }
+    };
+    if (isSignedIn) {
+      createUserDocument(); // Call the function to create user document
+    }
     if (isSignedIn && pathname === "/") {
       router.push("/dashboard"); // Redirect to dashboard only from the home page
     }
