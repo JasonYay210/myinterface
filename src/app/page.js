e5a42b0c93214a649521ae6adf2ffd987bb13e80
Jun <jz7259@g.rit.edu>
2025-02-23 11:57:52 -0500

    final changes before 12 brickhack

--- src/app/page.js
+++ src/app/page.js
@@ -16,7 +16,7 @@ import Button from "@/components/button";
 export default function Home() {
   return (
     <div>
-      <div className = "flex justify-between items-center gap-[50px] px-[100px] py-[75px] border-b border-black">
+      <div className = "flex justify-between items-center gap-[60px] px-[100px] py-[75px] border-b border-black">
         <div className = "flex flex-col gap-[15px]">
           <h1 className = "text-[27px] font-bold">BUILD SMARTER, OPTIMIZED WESBITES</h1>
           <p>An AI-driven tool ensures industry-standard UI/UX formatting and SEO, allowing you to focus on what truly matters — YOUR CONTENT.</p>
@@ -40,25 +40,28 @@ export default function Home() {
           </div>
           <p className = "text-[12px]">By Logging in, you accept to our <u>terms & conditions</u> and <u>privacy policies</u></p>
         </div>
-        <img className = "w-[500px]" src = "SEO Optimizer.png"></img>
+        <img className = "w-[500px]" src = "image.png"></img>
       </div>
       <div className = "flex flex-col items-center gap-[50px] px-[100px] py-[75px] border-b border-black">
         <div className = "flex justify-between gap-[15px] items-center">
           <h1 className = "text-[27px] w-full font-bold">BUILD SMARTER, OPTIMIZED WESBITES</h1>
           <p className = "border-r border-black px-[10px] text-right">We Use AI to standardizes text and image sizes based on  UI/UX industry standards.</p>
         </div>
-        <div className="flex gap-[25px] w-full">
-          <img className="h-auto w-1/2" src="SEO Optimizer.png" alt="SEO Optimizer" />
-          <img className="h-auto w-1/2" src="SEO Optimizer.png" alt="SEO Optimizer" />
+        <div className="border border-black rounded-[12px] overflow-hidden flex flex-col full">
+          <div className = "justify-end flex gap-[15px] px-[15px] border-b border-black w-full">
+            <p>o</p>
+            <p>o</p>
+          </div>
+          <img className="h-auto w-full" src="template.png" alt="SEO Optimizer" />
         </div>
       </div>
       <div className = "flex justify-between items-center gap-[50px] px-[100px] py-[75px] border-b border-black">
         <div className = "flex flex-col gap-[15px]">
-          <h1 className = "text-[27px] font-bold">WEBSITE LAYOUT IN MINUTES</h1>
+          <h1 className = "text-[27px] font-bold">WEBSITE MOCK UP IN SECONDS</h1>
           <p>Create Low-Fidelity Proof of Concept Wireframes in a matter of minutes. Optimize and standardize it with the click of a button.</p>
-          <a className = "px-[20px] py-[10px] w-fit bg-white text-black rounded-[8px] border border-black" href = "#">Learn More</a>
+          {/* <a className = "px-[20px] py-[10px] w-fit bg-white text-black rounded-[8px] border border-black" href = "#">Learn More</a> */}
         </div>
-        <img className = "w-[650px] border border-black rounded-[8px]" src = "myinterfaceformat.png"></img>
+        <img className = "w-[800px] border border-black rounded-[8px]" src = "Mockup.png"></img>
       </div>
       <Websitefooter/>
     </div>
--- src/app/sign-up/[[...sign-up]]/page.js
+++ src/app/sign-up/[[...sign-up]]/page.js
@@ -5,7 +5,13 @@ export default function SignUpPage(){
     return (
         <div className = "flex gap-[50px] w-full justify-center p-[50px] items-center">
             <SignUp />
-            <img className = "w-[500px] h-fit" src = "SEO Optimizer.png"></img>
+            <div className="border border-black rounded-[12px] overflow-hidden flex flex-col full">
+                <div className = "justify-end flex gap-[15px] px-[15px] border-b border-black w-full">
+                    <p>o</p>
+                    <p>o</p>
+                </div>
+                <img className="h-auto w-[700px]    " src="template.png" alt="SEO Optimizer" />
+            </div>
         </div>
     )
 }
